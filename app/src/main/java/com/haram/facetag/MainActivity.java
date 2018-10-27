2943b97613c8f0e50acc4f735e2e6f78696a10fb
bae haram <gkfkagkfka12@naver.com>
2018-10-27 13:28:22 +0900

    Add progressbar and input sex field into cloud firestore

--- app/src/main/java/com/haram/facetag/MainActivity.java
+++ app/src/main/java/com/haram/facetag/MainActivity.java
@@ -1,6 +1,8 @@
 package com.haram.facetag;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,20 +10,27 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText editText_email,editText_password;
     private String email, password;
     private FirebaseAuth auth;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +40,12 @@ public class MainActivity extends AppCompatActivity {
         editText_email = findViewById(R.id.main_edittext_email);
         editText_password = findViewById(R.id.main_edittext_password);
         auth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.main_progress);
+        progressBar.setVisibility(View.INVISIBLE);
+        progressBar.setIndeterminate(true);
+        progressBar.getIndeterminateDrawable()
+                .setColorFilter(Color.rgb(60,179,113), PorterDuff.Mode.MULTIPLY);
+
 
         Button button_register = findViewById(R.id.main_button_register);
         button_register.setOnClickListener(new View.OnClickListener() {
@@ -40,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
                 password = editText_password.getText().toString().trim();
 
                 if(isCompleted()){
+                    progressBar.setVisibility(View.VISIBLE);
                     registerUserByFirebase();
                 }
             }
@@ -53,12 +69,14 @@ public class MainActivity extends AppCompatActivity {
                 password = editText_password.getText().toString().trim();
 
                 if(isCompleted()){
+                    progressBar.setVisibility(View.VISIBLE);
                     loginByFirebase();
                 }
             }
         });
     }
 
+    // Firebase 인증으로 로그인
     private void loginByFirebase() {
         editText_email.setEnabled(false);
         editText_password.setEnabled(false);
@@ -67,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()){
-                    startActivity(new Intent(MainActivity.this, GenderActivity.class));
+                    startActivity(new Intent(MainActivity.this, SexActivity.class));
                     finish();
                 } else {
                     editText_email.setEnabled(true);
@@ -75,11 +93,62 @@ public class MainActivity extends AppCompatActivity {
                     try {
                         throw task.getException();
                     } catch(Exception e){
+                        progressBar.setVisibility(View.INVISIBLE);
+                        editText_email.setEnabled(true);
+                        editText_password.setEnabled(true);
                         Toast.makeText(MainActivity.this, getString(R.string.login_fail), Toast.LENGTH_SHORT).show();
                     }
                 }
             }
         });
+    }
+
+    // Firebase 인증으로 유저 등록
+    private void registerUserByFirebase() {
+        editText_email.setEnabled(false);
+        editText_password.setEnabled(false);
+
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            inputUserID();
+                        }
+                        else {
+                            progressBar.setVisibility(View.INVISIBLE);
+                            editText_email.setEnabled(true);
+                            editText_password.setEnabled(true);
+                            try {
+                                throw task.getException();
+                            } catch(FirebaseAuthUserCollisionException e) {
+                                editText_email.setError(getString(R.string.user_collision));
+                                editText_email.requestFocus();
+                            } catch(FirebaseAuthInvalidCredentialsException e) {
+                                editText_email.setError(getString(R.string.invalid_email));
+                                editText_email.requestFocus();
+                            } catch(Exception e){
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                });
+    }
+
+    // Cloud Firestore에 사용자 ID값 넣기
+    private void inputUserID() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        Map<String, String> gender = new HashMap<>();
+        gender.put("sex","");
+        db.collection("User").document(FirebaseAuth.getInstance().getCurrentUser().getUid()).set(gender)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(MainActivity.this, getString(R.string.register_complete), Toast.LENGTH_SHORT)
+                                .show();
+                        loginByFirebase();
+                    }
+                });
     }
 
     // 비어있는 부분이 없는지 확인
@@ -98,38 +167,5 @@ public class MainActivity extends AppCompatActivity {
             return false;
         }
         return true;
-    }
-
-    // Firebase 인증으로 유저 등록
-    private void registerUserByFirebase() {
-        editText_email.setEnabled(false);
-        editText_password.setEnabled(false);
-
-        auth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            Toast.makeText(MainActivity.this, getString(R.string.register_complete), Toast.LENGTH_SHORT)
-                                    .show();
-                            loginByFirebase();
-                        }
-                        else {
-                            editText_email.setEnabled(true);
-                            editText_password.setEnabled(true);
-                            try {
-                                throw task.getException();
-                            } catch(FirebaseAuthUserCollisionException e) {
-                                editText_email.setError(getString(R.string.user_collision));
-                                editText_email.requestFocus();
-                            } catch(FirebaseAuthInvalidCredentialsException e) {
-                                editText_email.setError(getString(R.string.invalid_email));
-                                editText_email.requestFocus();
-                            } catch(Exception e){
-                                e.printStackTrace();
-                            }
-                        }
-                    }
-                });
     }
 }
---
+++ app/src/main/java/com/haram/facetag/SexActivity.java
@@ -0,0 +1,77 @@
+package com.haram.facetag;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class SexActivity extends AppCompatActivity {
+
+    private FrameLayout male, female;
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sex);
+
+        male = findViewById(R.id.sex_male);
+        female = findViewById(R.id.sex_female);
+        progressBar = findViewById(R.id.sex_progress);
+
+        progressBar.setVisibility(View.INVISIBLE);
+        progressBar.setIndeterminate(true);
+        progressBar.getIndeterminateDrawable()
+                .setColorFilter(Color.rgb(60,179,113), PorterDuff.Mode.MULTIPLY);
+
+        male.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectSex(getString(R.string.field_sex_male));
+            }
+        });
+
+        female.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectSex(getString(R.string.field_sex_female));
+            }
+        });
+    }
+
+    private void selectSex(String sex) {
+        male.setEnabled(false);
+        female.setEnabled(false);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection(getString(R.string.collection_user)).document(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .update(getString(R.string.field_sex),sex)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        startActivity(new Intent(SexActivity.this, AnalyzeActivity.class));
+                        finish();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        progressBar.setVisibility(View.INVISIBLE);
+                        male.setEnabled(true);
+                        female.setEnabled(true);
+                        Toast.makeText(SexActivity.this, getString(R.string.invalid_input), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+}
